bba5d367878465494ff2f55bc486dbbe709fb064
adelina-chiriac <adelinachiriac@gmail.com>
2021-09-30 21:06:03 +0100

    Write a test to check for valid email submission

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -1,4 +1,5 @@
 const assert = require("assert");
+const render = require("../../render");
 
 it("Has a text input", async () => {
     const dom = await render("index.html");
@@ -6,4 +7,19 @@ it("Has a text input", async () => {
     const input = dom.window.document.querySelector("input");
     
     assert(input);
+});
+
+it("Shows a success message with a valid email", async () => {
+    const dom = await render("index.html");
+
+    const input = dom.window.document.querySelector("input");
+    input.value = "hello@email.com";
+
+    dom.window.document
+        .querySelector("form")
+        .dispatchEvent(new dom.window.Event("submit"));
+
+    const h1 = dom.window.document.querySelector("h1");
+
+    assert.strictEqual(h1.innerHTML, "Valid email :)");
 });
